3846597a657ad8214812cee25f2c380a6c4a65b0
Yoann Moreau <moreau.yo@gmail.com>
2012-07-27 15:59:37 +0200

    Added space insertion to prevent indexer to concatenate words when removing tags

--- src/parser/xmlparser.cpp
+++ src/parser/xmlparser.cpp
@@ -210,8 +210,6 @@ bool XMLParser::characters(const QString &ch)
     if(!_isParsing)
         return true;
 
-    //! \todo manage words separated by a tag only, insert space ?
-
     _dataManager.addText(ch, _insertSpace);
     return true;
 }
